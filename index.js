ad6b1db682f7f2093c208ea9092fd966addbce77
Helin <helintoptanci3@outlook.com>
2022-12-12 21:33:49 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+// remove the main#main element
+const main = document.querySelector('main#main');
+main.parentNode.removeChild(main);
+
+// create an <h1> element and assign it to the 'newHeader' variable
+const newHeader = document.createElement('h1');
+
+// set the id of the <h1> element to 'victory'
+newHeader.id = 'victory';
+
+// set the text of the <h1> element to "YOUR-NAME is the champion"
+newHeader.innerHTML = "helin is the champion";
